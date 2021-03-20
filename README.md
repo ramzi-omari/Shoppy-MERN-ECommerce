da03760fee13349893c9a1e66c9edcff69e81758
ramzi-omari <ramziomari1995@gmail.com>
2021-03-20 23:27:10 +0100

    Add colors to backend log

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - after changing in package scripts, npm start to run the server
 - npm i -D nodemon concurrently : & add scripts to use "npm run dev" to start server & react at the same time
 - npm i mongoose
+- npm i colors
 
 ## Frontend additional setup:
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // initialise Express
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 
 // import DataBase
 import connectDB from "./config/db.js";
@@ -34,5 +35,7 @@ const PORT = process.env.PORT || 5000;
 // listen to port
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );

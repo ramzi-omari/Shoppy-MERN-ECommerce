c06866ccddca3abd820e679f428c4beccf502eab
ramzi-omari <ramziomari1995@gmail.com>
2021-03-24 21:46:27 +0100

    Custom error handling -Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
-import express from "express";
-import asyncHandler from "express-async-handler";
-const router = express.Router();
-import Product from "../models/productModel.js";
+import express from "express"
+import asyncHandler from "express-async-handler"
+const router = express.Router()
+import Product from "../models/productModel.js"
 
 // @desc   fetch all Products
 // @route  GET /api/products
@@ -9,11 +9,11 @@ import Product from "../models/productModel.js";
 router.get(
   "/", // asynchandler to handle exception without writing trycatch everytime
   asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+    const products = await Product.find({})
 
-    res.json(products);
+    res.json(products)
   })
-);
+)
 
 // @desc   fetch single Product by id
 // @route  GET /api/products/:id
@@ -22,15 +22,16 @@ router.get(
   "/:id",
   asyncHandler(async (req, res) => {
     // req.params.id === get the :id from the link
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id)
 
     // check if the product exists
     if (product) {
-      res.json(product);
+      res.json(product)
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404)
+      throw new Error("Product not found")
     }
   })
-);
+)
 
-export default router;
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,40 @@
 // initialise Express
-import express from "express";
-import dotenv from "dotenv";
-import colors from "colors";
+import express from "express"
+import dotenv from "dotenv"
+import colors from "colors"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 // import DataBase
-import connectDB from "./config/db.js";
+import connectDB from "./config/db.js"
 
 // import routes
-import productRoutes from "./routes/productRoutes.js";
+import productRoutes from "./routes/productRoutes.js"
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
 // initialise app
-const app = express();
+const app = express()
 
 //firest route: Get request
 app.get("/", (req, res) => {
-  res.send("API is running");
-});
+  res.send("API is running")
+})
 
 // link to route
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes)
 
-const PORT = process.env.PORT || 5000;
+// custom error middleware
+app.use(notFound)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 // listen to port
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
   )
-);
+)

55c45cfe8f97765d29fa36b168db0ccfb8205bee
ramzi-omari <ramziomari1995@gmail.com>
2021-03-26 21:30:37 +0100

    Add to cart & redirect to Cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
 
 function App() {
   return (
@@ -14,11 +15,13 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          {/* id is optional in cart's link */}
+          <Route path="/cart/:id?" component={CartScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,7 +7,7 @@ import { listProductDetails } from "../actions/productAction"
 import Loader from "../components/Loader"
 import Message from "../components/Message"
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
   const [qty, setQty] = useState(0)
 
   const dispatch = useDispatch()
@@ -18,6 +18,10 @@ const ProductScreen = ({ match }) => {
   useEffect(() => {
     dispatch(listProductDetails(match.params.id))
   }, [dispatch, match])
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -94,6 +98,7 @@ const ProductScreen = ({ match }) => {
 
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className="btn-block"
                     type="button"
                     disabled={product.countInStock === 0}

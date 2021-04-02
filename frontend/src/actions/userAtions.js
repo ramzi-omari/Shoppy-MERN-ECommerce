09b326cb2b44d54617a07bc6b0c914337be6dc36
ramzi-omari <ramziomari1995@gmail.com>
2021-04-02 23:43:43 +0100

    Frontend/ User Login reducer Action

---
+++ frontend/src/actions/userAtions.js
@@ -0,0 +1,44 @@
+import axios from "axios"
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants"
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    })
+
+    const config = {
+      // to pass the token with protected routes
+      // send in the headers content type of application/json
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+
+    // make request
+    const { data } = await axios.post(
+      "api/users/login",
+      { email, password },
+      config
+    )
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    localStorage.setItem("userInfo", JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}

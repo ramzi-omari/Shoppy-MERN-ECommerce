9d2c5338735b87156ff3104b90daaaf4cea1842f
ramzi-omari <ramziomari1995@gmail.com>
2021-04-03 20:20:13 +0100

    Update Profile Endpoint 'Controller/Route'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,4 +87,35 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @desc   Update user profile
+// @route  PUT /api/users/profile
+// @access Private  // protected access
+const updateUserProfile = asyncHandler(async (req, res) => {
+  // req.user can be used to get the data from any PROTECTED Route
+  // get the user logged in
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    // check the password was sent
+    if (req.body.password) {
+      user.password = req.body.password || user.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      idAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }

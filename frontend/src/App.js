834ebbdb4ec41abd3918b54c58a6f082265e8679
ramzi-omari <ramziomari1995@gmail.com>
2021-04-03 22:22:05 +0100

    Profile Screen & getUserDetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
+import ProfileScreen from "./screens/ProfileScreen"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Container>
           <Route path="/login" component={LoginScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           {/* id is optional in cart's link */}
           <Route path="/cart/:id?" component={CartScreen}></Route>

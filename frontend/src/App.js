8f9ae97865613db281c1e7090a4061bc925882b0
ramzi-omari <ramziomari1995@gmail.com>
2021-04-03 19:51:03 +0100

    User Register Screen/Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/login" component={LoginScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
           {/* id is optional in cart's link */}
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
